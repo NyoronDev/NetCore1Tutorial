674d259dfd7007f54cc6a91afc980eaf321273d0
NyoronDev <nyoronsheppard@outlook.es>
2018-10-07 17:07:41 +0200

    First Exercise.

---
+++ Ch01_MyFirstApp/Ch01_MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ch01_MyFirstApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.ReadLine();
+        }
+    }
+}
