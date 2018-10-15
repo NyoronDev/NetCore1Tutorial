f12dbaedbdc6a45ef2480d28d755d9f7f9f7bbad
NyoronDev <nyoronsheppard@outlook.es>
2018-10-15 17:59:19 +0200

    Add Chapter8, Chapter9 and Chapter10

---
+++ Chapter09/Ch09_MyLINQExtensions/MyLINQExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch09_MyLINQExtensions
+{
+    public static class MyLINQExtensions
+    {
+        // This is a chainable LINQ extension method
+        public static IEnumerable<T> ProcessSequence<T>(this IEnumerable<T> sequence)
+        {
+            return sequence;
+        }
+    }
+}
