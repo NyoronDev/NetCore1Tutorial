016eba7ba99ad81fd42973d7e5dc389ef70b57e6
NyoronDev <nyoronsheppard@outlook.es>
2018-10-15 19:08:43 +0200

    Add Chapter 11.

---
+++ Chapter11/Encryption/Program.cs
@@ -0,0 +1,29 @@
+﻿using Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message that you want to encrypt: ");
+            var message = Console.ReadLine();
+
+            Console.WriteLine("Enter a password: ");
+            var password = Console.ReadLine();
+
+            var cryptoText = Protector.Encrypt(message, password);
+            Console.WriteLine($"Encrypted text: {cryptoText}");
+
+            var clearText = Protector.Decrypt(cryptoText, password);
+            Console.WriteLine($"Decrypted text: {clearText}");
+
+            Console.ReadLine();
+        }
+    }
+}
